5a46bc489a69b156fb64c233429bdae913cfb31c
Mac <mac@MacBook-Pro-de-Mac.local>
2018-03-26 16:36:15 -0500

    CreandoComponentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './componentes/homepage/homepage.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RegisterpageComponent } from './componentes/registerpage/registerpage.component';
+import { LoginpageComponent } from './componentes/loginpage/loginpage.component';
+import { PrivadopageComponent } from './componentes/privadopage/privadopage.component';
+import { NotfoundpageComponent } from './componentes/notfoundpage/notfoundpage.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    RegisterpageComponent,
+    LoginpageComponent,
+    PrivadopageComponent,
+    NotfoundpageComponent
   ],
   imports: [
     BrowserModule,
