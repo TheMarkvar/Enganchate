7abab2dfdc4484ccfefd14c135c8ecd9e452c991
Mac <mac@MacBook-Pro-de-Mac.local>
2018-03-26 19:08:24 -0500

    AvanceComponentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './componentes/homepage/homepage.component';
+import {LoginPageComponent} from './componentes/loginpage/loginpage.component';
+import {RegisterPageComponent} from './componentes/registerpage/registerpage.component';
+import {PrivadoPageComponent} from './componentes/privadopage/privadopage.component';
+import {NotFoundPageComponent} from './componentes/notfoundpage/notfoundpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'privado', component: PrivadoPageComponent},
+  {path: '**', component: NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
