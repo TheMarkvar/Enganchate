bf86eb0698c9e401ba6d70b31a48d6a24f1ffc91
Mac <mac@MacBook-Pro-de-Mac.local>
2018-03-26 19:26:10 -0500

    Avance Correcion Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomePageComponent} from './componentes/homepage/homepage.component';
-import {LoginPageComponent} from './componentes/loginpage/loginpage.component';
-import {RegisterPageComponent} from './componentes/registerpage/registerpage.component';
-import {PrivadoPageComponent} from './componentes/privadopage/privadopage.component';
-import {NotFoundPageComponent} from './componentes/notfoundpage/notfoundpage.component';
+import {HomepageComponent} from './componentes/homepage/homepage.component';
+import {LoginpageComponent} from './componentes/loginpage/loginpage.component';
+import {RegisterpageComponent} from './componentes/registerpage/registerpage.component';
+import {PrivadopageComponent} from './componentes/privadopage/privadopage.component';
+import {NotfoundpageComponent} from './componentes/notfoundpage/notfoundpage.component';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent},
-  {path: 'login', component: LoginPageComponent},
-  {path: 'register', component: RegisterPageComponent},
-  {path: 'privado', component: PrivadoPageComponent},
-  {path: '**', component: NotFoundPageComponent}
+  {path: '', component: HomepageComponent},
+  {path: 'login', component: LoginpageComponent},
+  {path: 'register', component: RegisterpageComponent},
+  {path: 'privado', component: PrivadopageComponent},
+  {path: '**', component: NotfoundpageComponent}
 ];
 
 @NgModule({
