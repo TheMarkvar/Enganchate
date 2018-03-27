0c4210ccd44d57895a55e9d05a53fc28485affc3
Carlos Rodríguez <andresrod1225@gmail.com>
2018-03-27 00:50:08 -0500

    Avance en registro de usuario firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
+//Componentes
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './componentes/homepage/homepage.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
@@ -11,6 +13,15 @@ import { LoginpageComponent } from './componentes/loginpage/loginpage.component'
 import { PrivadopageComponent } from './componentes/privadopage/privadopage.component';
 import { NotfoundpageComponent } from './componentes/notfoundpage/notfoundpage.component';
 
+//Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+//Environment
+import { environment } from '../environments/environment';
+
+//Servicios
+import { AuthService } from './servicios/auth.service';
 
 @NgModule({
   declarations: [
@@ -24,9 +35,12 @@ import { NotfoundpageComponent } from './componentes/notfoundpage/notfoundpage.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
